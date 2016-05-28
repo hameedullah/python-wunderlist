1e04f62896d7ac7dcdbbe539423d912aa390b15c
Hameedullah Khan <h@hameedullah.com>
2016-05-28 23:50:32 +0400

    Add support for Query params

--- tests/wlmock.py
+++ tests/wlmock.py
@@ -3,6 +3,7 @@
 
 from httmock import urlmatch, response
 import os
+import urllib.parse
 
 JSON_HEADER = {'Content-Type': 'application/json'}
 JSON_FIXTURES = os.path.join(os.path.dirname(os.path.realpath(__file__)), "fixtures")
@@ -11,6 +12,11 @@ JSON_FIXTURES = os.path.join(os.path.dirname(os.path.realpath(__file__)), "fixtu
 def api_endpoint(url, request):
     # TODO: Debug why os.path.join not working here
     json_path = JSON_FIXTURES + url.path
+    if url.query:
+        data = urllib.parse.parse_qsl(url.query)
+        query_path = os.path.join(data[0][0], data[0][1])
+        json_path = os.path.join(json_path, query_path)
+        
     if os.path.isdir(json_path):
         json_path = os.path.join(json_path, "index")
     try:
