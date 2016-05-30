e7d4c4caccc5edeff4a70fbfd451768e8ddee2be
Hameedullah Khan <h@hameedullah.com>
2016-05-30 15:55:32 +0400

    Rename project

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='python-wunderlist',
+setup(name='wunderlist',
       version='0.1',
       description='Wunderlist API in Python',
       url='http://github.com/hameedullah/python-wunderlist',
